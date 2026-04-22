02ecce6a471affe6edb56e6928b8b40e51c13491
coralmess <coralmess@gmail.com>
2026-04-22 17:50:02 +0300

    Megane

--- config_auto_ria_urls.py
+++ config_auto_ria_urls.py
@@ -3,4 +3,8 @@ AUTO_RIA_URLS = [
         "url": "https://auto.ria.com/uk/search/?search_type=1&category=1&all[0].any[0].brand=6&all[0].any[0].any[0].model=46&all[0].any[0].any[1].model=59425&all[0].any[0].year[0]=2015&all[0].any[0].year[1]=2017&price[0]=1&price[2]=13000&gearbox[0]=2&gearbox[1]=3&gearbox[2]=4&gearbox[3]=5&drive[0]=1&drive[1]=2&mileage[1]=170&engine_volume[0]=1.78&engine_volume[1]=2.1&abroad=0&customs_cleared=1&page=0&limit=20",
         "url_name": "Audi A3 Search",
     },
+    {
+        "url": "https://auto.ria.com/uk/search/?search_type=1&category=1&all[0].any[0].brand=62&all[0].any[0].any[0].model=586&all[0].any[0].any[0].any[0].generation=81&all[0].any[0].year[0]=2017&all[0].any[0].year[1]=2019&price[0]=1&price[2]=12000&gearbox[0]=2&gearbox[1]=3&gearbox[2]=4&gearbox[3]=5&mileage[1]=160&abroad=0&customs_cleared=1&page=0&limit=20",
+        "url_name": "Renault Megane IV Search",
+    }
 ]
